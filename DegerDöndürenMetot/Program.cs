6ece12509b1b0f5441a88c30181c131c239103f3
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-01-31 15:58:32 +0300

    Add files via upload

---
+++ DegerDöndürenMetot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DegerDöndürenMetot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Geriye İnteger Değer Döndüren Metot
+
+            int[] dizi = new int[10];
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                dizi[i] = rastgele();
+
+                Console.Write(dizi[i] + "  ");
+            }
+        }
+
+        static int rastgele()
+        {
+            Random r = new Random();
+            return r.Next(101);
+        }
+    }
+    }
+
